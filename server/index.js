0011e888560010332df4fafdcb9740cb8624d634
Asitha Navaratne <asithan476@gmail.com>
2024-06-18 10:04:24 +0530

    Created initial handshake and connection handlers

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const { WebSocketServer } = require("ws");
+
+const url = require("url");
+
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+
+const port = 8000;
+
+wsServer.on("connection", (connection, request) => {
+  const { username } = url.parse(request.url, true).query;
+
+  console.log(username);
+});
+
+server.listen(port, () => {
+  console.log(`WebSocket server running on port ${port}`);
+});
