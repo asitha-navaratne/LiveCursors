33e86f0b5d964fb6a6eb1b30351fd22ad49ed0d8
Asitha Navaratne <asithan476@gmail.com>
2024-06-22 13:21:43 +0530

    Broadcasting mouse pointer in ws connection

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useRef } from "react";
+import useWebSocket from "react-use-websocket";
+import throttle from "lodash.throttle";
+
 import PropTypes from "prop-types";
 
 const Home = ({ username }) => {
+  const WS_URL = "ws://127.0.0.1:8000";
+  const THROTTLE = 50;
+
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
+
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      sendJsonMessageThrottled.current({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    });
+  }, []);
+
   return <h1>Hello {username}</h1>;
 };
 
